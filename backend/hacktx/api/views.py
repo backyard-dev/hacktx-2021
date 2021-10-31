b1ca3b2f06a93cdafbd53259c2250fd5793def50
caleb-chou <haloisawesome412@gmail.com>
2021-10-30 23:20:40 -0500

    user auth

--- backend/hacktx/api/views.py
+++ backend/hacktx/api/views.py
@@ -1,10 +1,27 @@
+from os import name
 from django.shortcuts import render
-from firebase_admin import firestore, credentials
+from rest_framework.decorators import api_view
+from rest_framework import status
+from rest_framework.response import Response
+from firebase_admin import firestore, credentials, auth
 import firebase_admin
 
-# Create your views here.
-
 cred = credentials.Certificate('./auth.json')
-firebase_admin.initialize_app(cred)
+default_app = firebase_admin.initialize_app(cred)
 
 db = firestore.client()
+
+
+@api_view(['GET', 'POST'])
+def signup(request):
+    print(request)
+    email = request.data['email']
+    password = request.data['password']
+    print('Password: ' + str(password))
+    print('Email: ' + str(email))
+    try:
+        user = auth.create_user(email=email, password=password)
+        token = auth.create_custom_token(user.uid)
+        return Response(token, status=status.HTTP_200_OK)
+    except:
+        return Response('error', status=status.HTTP_400_BAD_REQUEST)
